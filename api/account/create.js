b2c7505430812b232d86e293e6bfff549e59e348
gize7 <gize.ajigo@gmail.com>
2019-09-05 10:48:45 +0300

    Checks if the email exists and returns an error

--- api/account/create.js
+++ api/account/create.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post("/", async (req, res) => {
 	const { email, name, age } = req.body;
 	let account = await Account.findOne({ email });
+	if (account) return res.status(400).send({ error: "email already exist" });
 	account = new Account({ email, name, age });
 	await account.save();
 	return res.send({ message: "success" });
